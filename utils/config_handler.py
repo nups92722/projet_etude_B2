b8620c31df626cc8454f9eba1062536b94de9b01
nicolas leprince <nicolas.leprince20@orange.fr>
2025-02-24 09:34:21 +0100

    ajout : commentaires sur le fichier config_handler

--- utils/config_handler.py
+++ utils/config_handler.py
@@ -3,16 +3,20 @@ import json
 
 class config_handler:
     def __init__(self):
+        # Initialize database configuration with default None values
         self.db = {
             'host': None,
             'user': None,
             'password': None,
             'database': None
         }
+
+        # Initialize file paths with default None values
         self.raw_data_csv = None
         self.raw_data_json = None
         self.parsed_data_json = None
 
+        # Check if the configuration file exists
         if os.path.exists("config.json"):
             print("Fichier de configuration existant ")
             self.load_config()
@@ -21,11 +25,14 @@ class config_handler:
             self.create_config()
 
     def load_config(self):
+        # Open and read the configuration file
         with open("config.json", 'r') as fichier:
             data = json.load(fichier)
 
+        # List to track missing keys
         missing_keys = []
 
+        # Check for required keys in the configuration
         for key in ['raw_data_csv', 'raw_data_json', 'parsed_data_json']:
             if key in data:
                 setattr(self, key, data[key])
@@ -41,10 +48,14 @@ class config_handler:
         else:
             missing_keys.append('database')
 
+        # If there are missing keys, raise an error
         if missing_keys:
             raise KeyError(f"Erreur de configuration : clés manquantes -> {', '.join(missing_keys)}")
+        else:
+            print("Fichier de configuration lu avec succès")
 
     def create_config(self):
+        # Prompt user for configuration details
         self.raw_data_csv = input("Entrez le chemin du fichier CSV brut : ")
         self.raw_data_json = input("Entrez le chemin du fichier JSON brut : ")
         self.parsed_data_json = input("Entrez le chemin du fichier JSON parsé : ")
@@ -54,6 +65,7 @@ class config_handler:
         self.db['password'] = input("Entrez le mot de passe de la base de données : ")
         self.db['database'] = input("Entrez le nom de la base de données : ")
 
+        # Create a dictionary with the configuration data
         config_data = {
             'raw_data_csv': self.raw_data_csv,
             'raw_data_json': self.raw_data_json,
@@ -61,12 +73,8 @@ class config_handler:
             'database': self.db
         }
 
+        # Write the configuration data to a JSON file
         with open("config.json", 'w') as fichier:
             json.dump(config_data, fichier, indent=4)
         
         print("Fichier de configuration créé avec succès")
-
-try :
- config = config_handler()
-except Exception as e:
-        print(f"Unexpected error: {e}")
